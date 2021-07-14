cd3110a02c869df99d2d139100b062438a4feb41
dip1683 <dip1683@gmail.com>
2021-07-14 07:29:33 +0530

    modified deployment.sh

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,17 @@
 docker build -t dip1683/multi-client:latest -t dip1683/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t dip1683/multi-server:latest -t dip1683/multi-server:$SHA -f ./server/Dockerfile ./server 
-docker build -t dip1683/multi-worker:latest -t dip1683/multi-worker:$SHA -f ./worker/Dockerfile ./worker 
+docker build -t dip1683/multi-server:latest -t dip1683/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t dip1683/multi-worker:latest -t dip1683/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push dip1683/multi-client:latest 
-docker push dip1683/multi-server:latest 
-docker push dip1683/multi-worker:latest 
+docker push dip1683/multi-client:latest
+docker push dip1683/multi-server:latest
+docker push dip1683/multi-worker:latest
 
-docker push dip1683/multi-client:$SHA 
-docker push dip1683/multi-server:$SHA 
-docker push dip1683/multi-worker:$SHA 
+docker push dip1683/multi-client:$SHA
+docker push dip1683/multi-server:$SHA
+docker push dip1683/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=dip1683/multi-server:$SHA 
-kubectl set image deployments/client-deployment client=dip1683/multi-client:$SHA 
-kubectl set image deployments/worker-deployment worker=dip1683/multi-worker:$SHA 
+kubectl set image deployments/server-deployment server=dip1683/multi-server:$SHA
+kubectl set image deployments/client-deployment client=dip1683/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=dip1683/multi-worker:$SHA
 
